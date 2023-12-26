f6dede881b4e7f65e8b0389c72b86ca6c86a3f0a
Andres Gallegos <gallegos-andres@hotmail.com>
2023-12-26 18:22:36 -0500

    se crea la vista modificar

---
+++ view/view_modificar.php
@@ -0,0 +1,58 @@
+<?php 
+include "../model/conexion.php";
+$id=$_GET["id"];
+$sql=$conexion->query("select * from carros where placa='$id'");
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modificar Vehículo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="col-4 p-3 m-auto">
+        <form method="POST">
+            <h2 class="text-center p-3">Modificar Vehículo</h2>
+            <?php
+            include "../controller/cont_modificar.php"; 
+            while($datos=$sql->fetch_object()){?>
+                <div class="mb-3">
+                <label for="inPutPlaca" class="form-label">Placa</label>
+                <input type="text" class="form-control" id="inPutPlaca" name="placa" value="<?= $datos->placa ?>">
+
+            </div>
+            <div class="mb-3">
+                <label for="inOutMarca" class="form-label">Marca</label>
+                <input type="text" class="form-control" id="inOutMarca" name="marca" value="<?= $datos->marca ?>">
+            </div>
+            <div class="mb-3">
+                <label for="inPutPlaca" class="form-label">Modelo</label>
+                <input type="text" class="form-control" id="inPutPlaca" name="modelo" value="<?= $datos->modelo ?>">
+
+            </div>
+            <div class="mb-3">
+                <label for="inOutMarca" class="form-label">Año</label>
+                <input type="text" class="form-control" id="inOutMarca" name="anio" value="<?= $datos->anio ?>">
+            </div>
+            <div class="mb-3">
+                <label for="inPutPlaca" class="form-label">Color</label>
+                <input type="text" class="form-control" id="inPutPlaca" name="color" value="<?= $datos->color ?>">
+
+            </div>
+            <?php
+            }?>            
+            <button type="submit" class="btn btn-primary" name="btnModificar" value="ok">Modificar</button>
+            <button type="submit" class="btn btn-secondary">Cancelar</button>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
